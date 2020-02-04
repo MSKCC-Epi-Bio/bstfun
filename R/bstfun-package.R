4f1475b77d32ed4e60937545ab6439d6d2b7027a
Daniel D. Sjoberg <danield.sjoberg@gmail.com>
2020-02-04 15:55:13 -0500

    first draft of tbl_ancova

--- R/bstfun-package.R
+++ R/bstfun-package.R
@@ -1,3 +1,8 @@
+#' @importFrom purrr %||% map imap map_chr map2 pmap
+#' @importFrom dplyr select mutate
+#' @importFrom forcats fct_rev as_factor
+#' @importFrom stringr fixed str_replace
+#' @importFrom rlang := .data .env
 #' @keywords internal
 "_PACKAGE"
 
