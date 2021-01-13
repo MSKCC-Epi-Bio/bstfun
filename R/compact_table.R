67aa2ec2343bd44e94d640d238cbb316fef7dfd3
Daniel D. Sjoberg <danield.sjoberg@gmail.com>
2021-01-13 14:34:07 -0500

    Update compact_table.R

--- R/compact_table.R
+++ R/compact_table.R
@@ -13,13 +13,13 @@
 #'   gt::gt() %>%
 #'   compact_table()
 compact_table <- function(data) {
-  # first extract the names of the commands that make tables compact
+  # first extract the theme element names of the commands that make tables compact
   # from the `gtsummary::theme_gtsummary_compact()` theme function
-  if (inherits(data, "gt_tbl")) cmd_names <- "as_gt-lst:addl_cmds"
-  else if (inherits(data, "flextable")) cmd_names <- "as_flex_table-lst:addl_cmds"
-  else if (inherits(data, "huxtable")) cmd_names <- "as_hux_table.gtsummary-lst:addl_cmds"
+  if (inherits(data, "gt_tbl")) theme_name <- "as_gt-lst:addl_cmds"
+  else if (inherits(data, "flextable")) theme_name <- "as_flex_table-lst:addl_cmds"
+  else if (inherits(data, "huxtable")) theme_name <- "as_hux_table.gtsummary-lst:addl_cmds"
   else if (inherits(data, "knitr_kable")) {
-    cmd_names <- "as_kable_extra-lst:addl_cmds"
+    theme_name <- "as_kable_extra-lst:addl_cmds"
     if (!requireNamespace("kableExtra"))
       rlang::abort("Styling a `knitr::kable()` table requires the {kableExtra} package.")
   }
@@ -29,7 +29,7 @@ compact_table <- function(data) {
   # extract the compact command expressions and
   # concatenate them into a single expression and evaluate
   gtsummary::theme_gtsummary_compact(set_theme = FALSE) %>%
-    purrr::pluck(cmd_names) %>%
+    purrr::pluck(theme_name) %>%
     unlist() %>%
     purrr::compact() %>% # removes null elements of list (there shouldn't be any)
     {c(list(data), .)} %>%
