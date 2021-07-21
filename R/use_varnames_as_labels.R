81f92a80df5fc88a65bfedde5341a4e939c92eaa
Brown <BrownS7@mskcc.org>
2021-07-21 17:16:20 -0400

    Updated use_varnames_as_labels documentation

--- R/use_varnames_as_labels.R
+++ R/use_varnames_as_labels.R
@@ -1,41 +1,54 @@
-#' Title
+#' Use Variable Names as Labels
 #'
-#' @param data
-#' @param caps
-#' @param exclude
+#' @param data a dataframe
+#' @param caps variables to be entirely capitalized
+#' @param exclude variables to exclude from labeling
 #'
-#' @return
+#' @return a labeled dataframe
 #' @export
 #'
 #' @examples
+#' library(gtsummary)
+#'
+#' mtcars %>%
+#'   use_varnames_as_labels(caps = vs, exclude = mpg) %>%
+#'   tbl_summary() %>%
+#'   as_kable()
 use_varnames_as_labels <- function(data, caps = NULL, exclude = NULL) {
+  exclude <- data %>%
+    select({{ exclude }}) %>%
+    names()
 
-  exclude = data %>% select({{ exclude }}) %>% names()
+  caps <- data %>%
+    select({{ caps }}) %>%
+    names() %>%
+    setdiff(exclude)
 
-  caps = data %>% select({{ caps }}) %>% names() %>% setdiff(exclude)
+  title <- setdiff(names(data), c(caps, exclude))
 
-  title_names = setdiff(names(data), c(caps, exclude))
-
-  title_labels = list()
+  label_list <- list()
 
   if (!rlang::is_empty(title_names)) {
-    title_labels <- stringr::str_replace_all(title_names, "_", " ") %>%
+    label_list <- stringr::str_replace_all(title, "_", " ") %>%
       stringr::str_to_lower(.) %>%
       stringr::str_to_title(.) %>%
       as.list() %>%
-      setNames(title_names) %>%
-      c(title_labels)
+      stats::setNames(title) %>%
+      c(label_list)
   }
 
   if (!rlang::is_empty(caps)) {
-    title_labels <- stringr::str_to_upper(caps) %>%
+    label_list <- stringr::str_to_upper(caps) %>%
       as.list() %>%
-      setNames(caps) %>%
-      c(title_labels)
+      stats::setNames(caps) %>%
+      c(label_list)
   }
 
-  data_labelled <- data %>% labelled::set_variable_labels(.labels = title_labels,
-                                                      .strict = FALSE)
+  data_labelled <- data %>%
+    labelled::set_variable_labels(
+      .labels = label_list,
+      .strict = FALSE
+    )
 
   return(data_labelled)
 }
