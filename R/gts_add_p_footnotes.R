854db4c19c889f0936137f18389678845ae07722
Lavery <laveryj@mskcc.org>
2021-10-01 10:33:51 -0400

    deprecate tbl_split() and gts_add_p_footnotes() since moving to gtsummary package

--- R/gts_add_p_footnotes.R
+++ R/gts_add_p_footnotes.R
@@ -19,68 +19,10 @@
 #' \if{html}{\figure{gts_add_p_footnotes_ex1.png}{options: width=80\%}}
 
 gts_add_p_footnotes <- function(x, printer = NULL, index_start = NULL) {
-  # check inputs ---------------------------------------------------------------
-  if (!inherits(x, "gtsummary") || !"p.value" %in% names(x$table_body))
-    stop("x must be a gtsummary table with a p-value column.", call. = FALSE)
-  if (!"stat_test_lbl" %in% names(x$meta_data))
-    stop("The `x$meta_data` data frame must have a column called 'stat_test_lbl'.")
+  lifecycle::deprecate_warn(when = "0.2.5", what = "bstfun::gts_add_p_footnotes()",
+                            with = "gtsummary::separate_p_footnotes()")
 
-  if (!is.null(printer) || !is.null(index_start))
-    paste("Arguments `printer=` and `index_start=` are deprecated and were ignored.",
-          "gtsummary table is no longer converted to gt or flextable; rather,",
-          "it maintains its gtsummary class.") %>%
-    message()
-
-  # remove p-value column footnote ---------------------------------------------
-  x <- gtsummary::modify_footnote(x, p.value ~ NA_character_)
-
-  # add footnotes to the body of the table -------------------------------------
-  footnote_calls <-
-    x$meta_data %>%
-    dplyr::select(.data$variable, .data$stat_test_lbl) %>%
-    tibble::deframe() %>%
-    purrr::imap(
-      ~rlang::expr(
-        gtsummary::modify_table_styling(columns = "p.value",
-                                        rows = .data$variable %in% !!.y & !is.na(.data$p.value),
-                                        footnote = !!.x)
-      )
-    )
-
-  # concatenating expressions with %>% between each of them
-  footnote_calls %>%
-    purrr::reduce(function(x, y) rlang::expr(!!x %>% !!y), .init = rlang::expr(!!x)) %>%
-    # evaluating expressions
-    eval()
-}
-
-
-#' Check that a package is installed, stopping otherwise
-#'
-#' @param pkg Package required
-#' @param fn Calling function from the user perspective
-#'
-#' @family gtsummary-related functions
-#' @return Returns NULL or not at all.
-#'
-#' @noRd
-#' @keywords internal
-assert_package <- function(pkg, fn, version = NULL) {
-  if (is.null(version) && !requireNamespace(pkg, quietly = TRUE)) {
-    ui_oops("The {ui_value(pkg)} package is required for function {ui_code(fn)}.")
-    usethis::ui_todo("Install {ui_value(pkg)} with the code below.")
-    ui_code_block('install.packages("{pkg}")')
-    stop("Install required package", call. = FALSE)
-  }
-
-  if (!is.null(version) &&
-      (!requireNamespace(pkg, quietly = TRUE) ||
-       (requireNamespace(pkg, quietly = TRUE) && utils::packageVersion(pkg) < version))) {
-    ui_oops("The {ui_value(pkg)} package v{version} or greater is required for function {ui_code(fn)}.")
-    usethis::ui_todo("Install/update {ui_value(pkg)} with the code below.")
-    ui_code_block('install.packages("{pkg}")')
-    stop("Install required package", call. = FALSE)
-  }
+  gtsummary::separate_p_footnotes(x)
 }
 
 
--- R/tbl_split.R
+++ R/tbl_split.R
@@ -23,37 +23,12 @@ NULL
 #' @export
 #' @rdname tbl_split
 tbl_split <- function(x, variables) {
-  # check/parse inputs ---------------------------------------------------------
-  if (!inherits(x, "gtsummary"))
-    stop("`x=` must be class 'gtsummary'", call. = FALSE)
+  lifecycle::deprecate_warn(when = "0.2.5", what = "bstfun::tbl_split()",
+                            with = "gtsummary::tbl_split()")
 
-  variables <-
-    broom.helpers::.select_to_varnames(
-      {{ variables }},
-      var_info = x$table_body,
-      arg_name = "variable"
-    ) %>%
-    # adding last variable
-    union(x$table_body$variable %>% rev() %>% purrr::pluck(1))
+  gtsummary::tbl_split(x, variables)
 
-  # merging split points -------------------------------------------------------
-  # convert list of table_body into list of gtsummary obejcts
-  x$table_body %>%
-    dplyr::left_join(
-      tibble(variable = variables, ..group.. = variables),
-      by = "variable"
-    ) %>%
-    tidyr::fill(.data$..group.., .direction = "up") %>%
-    tidyr::nest(data = -.data$..group..) %>%
-    dplyr::pull(.data$data) %>%
-    purrr::map(
-      ~list(.) %>%
-        purrr::set_names("table_body") %>%
-        c(purrr::list_modify(x, "table_body" = NULL)) %>% # add the other parts of the gtsummary table
-        `class<-`(class(x)) # add original class from `x`
-    ) %>%
-    `class<-`("tbl_split") # assign class (can't assign gtsummary becuase of print definitions)
-}
+  }
 
 #' @export
 #' @rdname tbl_split
