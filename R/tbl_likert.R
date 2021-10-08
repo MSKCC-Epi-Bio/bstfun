679927bc1875fb9acfa1b95cde8a43310a44b81f
Sjoberg <SjobergD@mskcc.org>
2021-10-07 21:02:35 -0400

    in progress

--- R/tbl_likert.R
+++ R/tbl_likert.R
@@ -1,32 +1,37 @@
-
-
+#' Likert Summary
+#'
+#' @export
 tbl_likert <- function(data) {
-  # making all variable factors with the same levels ---------------------------
-  browser()
-  levels <-
-    data %>%
+  # all variables in data must be a factor with the same levels ----------------
+  data %>%
     map(
       function(x) {
-        if (inherits(x, "factor"))
-          return(attr(x, "levels"))
-        else
-          return(unique(x) %>% sort() %>% as.character())
-      }
-    ) %>%
-    purrr::reduce(union) %>%
-    as.list()
-
-  data <-
-    data %>%
-    purrr::map_dfc(
-      function(x) {
-        browser()
-        if (inherits(x, "factor"))
-          return(forcats::fct_expand(x, !!!levels))
-        else
-          return(factor(x) %>% forcats::fct_expand(!!!levels))
+        if (!inherits(x, "factor"))
+          stop("All columns in `data=` must be class 'factor'.", call. = FALSE)
+        if (!identical(attr(x, "levels"), attr(data[[1]], "levels")))
+          stop("All columns must have the same factor levels.")
       }
     )
 
-
+  # create summary table, then stack -------------------------------------------
+  names(data) %>%
+    setdiff("...all_true...") %>%
+    purrr::map(
+      ~ gtsummary::tbl_summary(
+          data = data %>% dplyr::mutate(...all_true... = TRUE),
+          by = all_of(.x),
+          include = ...all_true...,
+          type = list(...all_true... = "dichotomous"),
+          percent = "row",
+          label = list(...all_true... = (attr(data[[.x]], "label") %||% .x))
+        ) %>%
+        gtsummary::modify_header(gtsummary::all_stat_cols() ~ "**{level}**") %>%
+        gtsummary_butcher() %>%
+        gtsummary::modify_table_body(
+          function(table_body) {
+            table_body %>% dplyr::mutate(variable = .x)
+          }
+        )
+    ) %>%
+    gtsummary::tbl_stack(quiet = TRUE)
 }
