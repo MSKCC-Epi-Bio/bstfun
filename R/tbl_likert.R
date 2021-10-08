93c5460cf1eaab69988ae0e9466760540d5a2c0f
Sjoberg <SjobergD@mskcc.org>
2021-10-07 21:28:27 -0400

    updates

--- R/tbl_likert.R
+++ R/tbl_likert.R
@@ -1,8 +1,48 @@
 #' Likert Summary
 #'
+#' @inheritParams gtsummary::tbl_summary
 #' @export
-tbl_likert <- function(data) {
+#' @examples
+#' df <-
+#'   tibble::tibble(
+#'     f1 =
+#'       sample.int(100, n = 3, replace = TRUE) %>%
+#'       factor(levels = 1:3, labels = c("bad", "meh", "good")),
+#'     f2 =
+#'       sample.int(100, n = 3, replace = TRUE) %>%
+#'       factor(levels = 1:3, labels = c("bad", "meh", "good")),
+#'   )
+#'
+#' tbl_likert(df) %>%
+#'   gtsummary::as_kable()
+tbl_likert <- function(data,
+                       label = NULL, statistic = NULL, digits = NULL,
+                       include = everything()) {
+  # evaluate inputs ------------------------------------------------------------
+  include <-
+    broom.helpers::.select_to_varnames( {{ include }},
+                                        data = data, arg_name = "include")
+  label <-
+    broom.helpers::.formula_list_to_named_list(
+      label,
+      data = data,
+      arg_name = "label"
+    )
+  statistic <-
+    broom.helpers::.formula_list_to_named_list(
+      statistic,
+      data = data,
+      arg_name = "statistic"
+    )
+  digits <-
+    broom.helpers::.formula_list_to_named_list(
+      digits,
+      data = data,
+      arg_name = "digits"
+    )
+
   # all variables in data must be a factor with the same levels ----------------
+  data <- data[include]
   data %>%
     map(
       function(x) {
@@ -22,8 +62,10 @@ tbl_likert <- function(data) {
           by = all_of(.x),
           include = ...all_true...,
           type = list(...all_true... = "dichotomous"),
+          statistic = list(...all_true... = statistic[[.x]]),
           percent = "row",
-          label = list(...all_true... = (attr(data[[.x]], "label") %||% .x))
+          digits = list(...all_true... = digits[[.x]]),
+          label = list(...all_true... = (label[[.x]] %||% attr(data[[.x]], "label") %||% .x))
         ) %>%
         gtsummary::modify_header(gtsummary::all_stat_cols() ~ "**{level}**") %>%
         gtsummary_butcher() %>%
