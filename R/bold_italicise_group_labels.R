c916c228e9b82dc715242221e7c5f3dfb2003dc5
Sjoberg <SjobergD@mskcc.org>
2021-10-23 17:37:40 -0400

    Update bold_italicise_group_labels.R

--- R/bold_italicise_group_labels.R
+++ R/bold_italicise_group_labels.R
@@ -2,23 +2,25 @@
 # to modify group labels. x$table_styling structure should be
 # modified as well?
 
-# bold_group_labels <- function(x) {
-#   updated_call_list <- c(x$call_list, list(bold_labels = match.call()))
-#   # input checks ---------------------------------------------------------------
-#   if (!inherits(x, "gtsummary")) {
-#     stop("Class of 'x' must be 'gtsummary'", call. = FALSE)
-#   }
-#
-#   # bold labels ----------------------------------------------------------------
-#   x <-
-#     modify_table_styling(
-#       x,
-#       columns = "label",
-#       rows = .data$row_type == "label",
-#       text_format = "bold"
-#     )
-#
-#   x$call_list <- updated_call_list
-#
-#   x
-# }
+bold_group_labels <- function(x, print_engine = c("gt", "flextable", "huxtable")) {
+  # input checks ---------------------------------------------------------------
+  if (!inherits(x, "gtsummary")) {
+    stop("Class of 'x' must be 'gtsummary'", call. = FALSE)
+  }
+  print_engine <- match.arg(print_engine)
+
+  # convert tbl to output type -------------------------------------------------
+  result <-
+    swtich(
+      print_engine,
+      "gt" = gtsummary::as_gt(x),
+      "flextable" = gtsummary::as_flex_table(x),
+      "huxtable" = gtsummary::as_hux_table(x),
+    )
+
+  # apply bold code to table ---------------------------------------------------
+  # THE GROUP COLUMNS WILL ALWAYS BEGIN WITH 'groupname_col*'
+  # APPLY THE FORMATTING TO THOSE COLUMNS
+
+  result
+}
