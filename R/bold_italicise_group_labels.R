75d1bcca7534045b18e3fedc2133ae22b6cbc523
oranwutang <gzapatawainberg@gmail.com>
2021-10-25 19:56:15 +0200

    adding functions for gt print engine

--- R/bold_italicise_group_labels.R
+++ R/bold_italicise_group_labels.R
@@ -1,6 +1,3 @@
-# I guess that a function like bold_labels() may be adapted
-# to modify group labels. x$table_styling structure should be
-# modified as well?
 
 bold_group_labels <- function(x, print_engine = c("gt", "flextable", "huxtable")) {
   # input checks ---------------------------------------------------------------
@@ -11,7 +8,7 @@ bold_group_labels <- function(x, print_engine = c("gt", "flextable", "huxtable")
 
   # convert tbl to output type -------------------------------------------------
   result <-
-    swtich(
+    switch(
       print_engine,
       "gt" = gtsummary::as_gt(x),
       "flextable" = gtsummary::as_flex_table(x),
@@ -22,6 +19,52 @@ bold_group_labels <- function(x, print_engine = c("gt", "flextable", "huxtable")
   # apply bold code to table ---------------------------------------------------
   # THE GROUP COLUMNS WILL ALWAYS BEGIN WITH 'groupname_col*'
   # APPLY THE FORMATTING TO THOSE COLUMNS
+  switch(
+    print_engine,
+    # "gt" = gtsummary::as_gt(x),
+    "gt" = {
+      result <-
+        result %>%
+        gt::tab_options(row_group.font.weight = "bold")
+           },
+    # "flextable" = {},
+    # "huxtable" =  {},
+  )
+  result
+}
 
+
+
+italicise_group_labels <- function(x, print_engine = c("gt", "flextable", "huxtable")) {
+  # input checks ---------------------------------------------------------------
+  if (!inherits(x, "gtsummary")) {
+    stop("Class of 'x' must be 'gtsummary'", call. = FALSE)
+  }
+  print_engine <- match.arg(print_engine)
+
+  # convert tbl to output type -------------------------------------------------
+  result <-
+    switch(
+      print_engine,
+      "gt" = gtsummary::as_gt(x),
+      "flextable" = gtsummary::as_flex_table(x),
+      "huxtable" = gtsummary::as_hux_table(x),
+    )
+  cli::cli_alert_info("{.field gtsummary} table has been converted class {.val {print_engine}}")
+
+  # apply bold code to table ---------------------------------------------------
+  # THE GROUP COLUMNS WILL ALWAYS BEGIN WITH 'groupname_col*'
+  # APPLY THE FORMATTING TO THOSE COLUMNS
+  switch(
+    print_engine,
+    # "gt" = gtsummary::as_gt(x),
+    "gt" = {
+      result <-
+        result %>%
+        gt::tab_style(style = cell_text(style = "italic"), locations = cells_row_groups())
+    },
+    # "flextable" = {},
+    # "huxtable" =  {},
+  )
   result
 }
