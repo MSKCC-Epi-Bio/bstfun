d26c51333403246256bbe7dced9aebc1e57eddd6
Daniel Sjoberg <danield.sjoberg@gmail.com>
2021-11-10 15:25:01 -0500

    updates to LEE template

--- data-raw/project_template.R
+++ data-raw/project_template.R
@@ -134,59 +134,29 @@ attr(whitingk_project_template, "label") <- "Karissa's Project Template"
 
 # leej22 template ------------------------------------------------------------
 leej22_project_template <-
-  quote(list(
-    readme = list(
-      template_filename = fs::path_package("project_templates/readme.md", package = 'bstfun'),
-      filename = "README.md",
-      copy = FALSE
-    ),
-    gitignore = list(
-      template_filename = fs::path_package("project_templates/leej22_template_files/gitignore.txt", package = 'bstfun'),
-      filename = ".gitignore",
-      copy = TRUE
-    ),
-    data_date = list(
-      template_filename = fs::path_package("project_templates/data_date.txt", package = 'bstfun'),
-      filename = "data_date.txt",
-      copy = FALSE
-    ),
-    setup = list(
+  project_template_default[c("readme", "gitignore", "data_date", "rproj", "rprofile")] |>
+  purrr::list_modify(
+    setup = rlang::expr(list(
       template_filename = fs::path_package("project_templates/leej22_template_files/01_data_setup.Rmd", package = 'bstfun'),
       filename = glue::glue("code/{folder_name}_01_data_setup.Rmd"),
       copy = FALSE
-    ),
-    data_checks = list(
+    )),
+    data_checks = rlang::expr(list(
       template_filename = fs::path_package("project_templates/leej22_template_files/02_data_checks.Rmd", package = 'bstfun'),
       filename = glue::glue("code/{folder_name}_02_data_checks.Rmd"),
       copy = FALSE
-    ),
+    )),
     analysis = list(
       template_filename = fs::path_package("project_templates/leej22_template_files/03_analysis.Rmd", package = 'bstfun'),
       filename = glue::glue("code/{folder_name}_03_analysis.Rmd"),
       copy = FALSE
     ),
-    report = list(
+    report = rlang::expr(list(
       template_filename = fs::path_package("project_templates/leej22_template_files/04_report.Rmd", package = 'bstfun'),
       filename = glue::glue("code/{folder_name}_04_report.Rmd"),
       copy = FALSE
-    ),
-    rproj = list(
-      template_filename = fs::path_package("project_templates/default_rproj.Rproj", package = 'bstfun'),
-      filename = glue::glue("{folder_name}.Rproj"),
-      copy = TRUE
-    ),
-    # only add Rprofile if renv was used
-    rprofile =
-      switch(
-        renv,
-        list(
-          template_filename =
-            fs::path_package(package = "starter", "project_templates/default_rprofile.R"),
-          filename = stringr::str_glue(".Rprofile"),
-          glue = TRUE
-        )
-      )
-  ))
+    ))
+  )
 attr(leej22_project_template, "script_path") <-
   expression(fs::path_package("project_templates/leej22_template_files/write_directories.R", package = 'bstfun'))
 attr(leej22_project_template, "label") <- "Jasme Lee Project Template"
