7a160748014659a992f2a6c20a4cd30888f81b6e
Daniel Sjoberg <danield.sjoberg@gmail.com>
2021-11-16 13:30:42 -0500

    Template updates (#75)
    * template updates
    
    * increment version number
    
    * updates to LEE template
    
    * updates to whitingk template
    
    * add biostatR and new label function from bstfun
    
    Co-authored-by: karissawhiting <karissa.whiting@gmail.com>

--- data-raw/project_template.R
+++ data-raw/project_template.R
@@ -64,22 +64,8 @@ attr(project_template_default, "label") <- "Default Biostistics Project Template
 
 # hot template -----------------------------------------------------------------
 hot_project_template <-
-  list(
-    readme = rlang::expr(list(
-      template_filename = fs::path_package("project_templates/hot_template_files/hot_readme.md", package = 'bstfun'),
-      filename = "README.md",
-      copy = FALSE
-    )),
-    gitignore = rlang::expr(list(
-      template_filename = fs::path_package("project_templates/gitignore.txt", package = 'bstfun'),
-      filename = ".gitignore",
-      copy = TRUE
-    )),
-    data_date = rlang::expr(list(
-      template_filename = fs::path_package("project_templates/data_date.txt", package = 'bstfun'),
-      filename = "data_date.txt",
-      copy = FALSE
-    )),
+  project_template_default[c("readme", "gitignore", "data_date", "references", "doc_template", "rproj", "rprofile")] |>
+  purrr::list_modify(
     setup = rlang::expr(list(
       template_filename = fs::path_package("project_templates/hot_template_files/hot_setup.Rmd", package = 'bstfun'),
       filename = glue::glue("scripts/setup1_{stringr::str_split(folder_name, pattern = ' |-', simplify = T)[, 1] %>% tolower()}.Rmd"),
@@ -110,144 +96,63 @@ hot_project_template <-
       filename = glue::glue("scripts/derived_variables_{stringr::str_split(folder_name, pattern = ' |-', simplify = T)[, 1] %>% tolower()}.xlsx"),
       copy = TRUE
     )),
-    rproj = rlang::expr(list(
-      template_filename = fs::path_package("project_templates/default_rproj.Rproj", package = 'bstfun'),
-      filename = glue::glue("_rstudio_project.Rproj"),
-      copy = TRUE
-    )),
-    bib = rlang::expr(list(
-      template_filename = fs::path_package("project_templates/references.bib", package = 'bstfun'),
-      filename = "scripts/templates/references.bib",
-      copy = TRUE
-    )),
     csl = rlang::expr(list(
       template_filename = fs::path_package("project_templates/hot_template_files/european-urology.csl", package = 'bstfun'),
       filename = "scripts/templates/european-urology.csl",
       copy = TRUE
-    )),
-    # only add Rprofile if renv was used
-    rprofile =
-      rlang::expr(switch(
-        renv,
-        list(
-          template_filename =
-            fs::path_package("project_templates/default_rprofile.R", package = "starter"),
-          filename = stringr::str_glue(".Rprofile"),
-          glue = TRUE
-        )
-      ))
+    ))
   )
 
 attr(hot_project_template, "script_path") <-
   rlang::expr(fs::path_package("project_templates/hot_template_files/hot_script.R", package = 'bstfun'))
-attr(project_template, "label") <- "H.O.T. Project Template"
+attr(hot_project_template, "label") <- "H.O.T. Project Template"
 
 # whitingk template ------------------------------------------------------------
 whitingk_project_template <-
-  quote(list(
-    readme = list(
-      template_filename = fs::path_package("project_templates/readme.md", package = 'bstfun'),
-      filename = "README.md",
-      copy = FALSE
-    ),
-    gitignore = list(
-      template_filename = fs::path_package("project_templates/gitignore.txt", package = 'bstfun'),
-      filename = ".gitignore",
-      copy = TRUE
-    ),
-    setup = list(
+  project_template_default[c("readme", "gitignore", "rproj", "rprofile")] |>
+  purrr::list_modify(
+    setup = rlang::expr(list(
       template_filename = fs::path_package("project_templates/whitingk_template_files/01_clean-data.R", package = 'bstfun'),
       filename = glue::glue("scripts/01_clean-data.R"),
       copy = FALSE
-    ),
-    analysis = list(
+    )),
+    analysis = rlang::expr(list(
       template_filename = fs::path_package("project_templates/whitingk_template_files/02_analysis.Rmd", package = 'bstfun'),
       filename = glue::glue("scripts/02_analysis.Rmd"),
       copy = FALSE
-    ),
-    report = list(
-      template_filename = fs::path_package("project_templates/whitingk_template_files/03_report.Rmd", package = 'bstfun'),
-      filename = glue::glue("scripts/03_report.Rmd"),
-      copy = FALSE
-    ),
-    rproj = list(
-      template_filename = fs::path_package("project_templates/default_rproj.Rproj", package = 'bstfun'),
-      filename = glue::glue("_rstudio_project.Rproj"),
-      copy = TRUE
-    ),
-    # only add Rprofile if renv was used
-    rprofile =
-      switch(
-        renv,
-        list(
-          template_filename =
-            fs::path_package(package = "starter", "project_templates/default_rprofile.R"),
-          filename = stringr::str_glue(".Rprofile"),
-          glue = TRUE
-        )
-      )
-  ))
+    ))
+  )
+
 attr(whitingk_project_template, "script_path") <-
   expression(fs::path_package("project_templates/whitingk_template_files/write_directories.R", package = 'bstfun'))
 attr(whitingk_project_template, "label") <- "Karissa's Project Template"
 
 
 # leej22 template ------------------------------------------------------------
-
 leej22_project_template <-
-  quote(list(
-    readme = list(
-      template_filename = fs::path_package("project_templates/readme.md", package = 'bstfun'),
-      filename = "README.md",
-      copy = FALSE
-    ),
-    gitignore = list(
-      template_filename = fs::path_package("project_templates/leej22_template_files/gitignore.txt", package = 'bstfun'),
-      filename = ".gitignore",
-      copy = TRUE
-    ),
-    data_date = list(
-      template_filename = fs::path_package("project_templates/data_date.txt", package = 'bstfun'),
-      filename = "data_date.txt",
-      copy = FALSE
-    ),
-    setup = list(
+  project_template_default[c("readme", "gitignore", "data_date", "rproj", "rprofile")] |>
+  purrr::list_modify(
+    setup = rlang::expr(list(
       template_filename = fs::path_package("project_templates/leej22_template_files/01_data_setup.Rmd", package = 'bstfun'),
       filename = glue::glue("code/{folder_name}_01_data_setup.Rmd"),
       copy = FALSE
-    ),
-    data_checks = list(
+    )),
+    data_checks = rlang::expr(list(
       template_filename = fs::path_package("project_templates/leej22_template_files/02_data_checks.Rmd", package = 'bstfun'),
       filename = glue::glue("code/{folder_name}_02_data_checks.Rmd"),
       copy = FALSE
-    ),
+    )),
     analysis = list(
       template_filename = fs::path_package("project_templates/leej22_template_files/03_analysis.Rmd", package = 'bstfun'),
       filename = glue::glue("code/{folder_name}_03_analysis.Rmd"),
       copy = FALSE
     ),
-    report = list(
+    report = rlang::expr(list(
       template_filename = fs::path_package("project_templates/leej22_template_files/04_report.Rmd", package = 'bstfun'),
       filename = glue::glue("code/{folder_name}_04_report.Rmd"),
       copy = FALSE
-    ),
-    rproj = list(
-      template_filename = fs::path_package("project_templates/default_rproj.Rproj", package = 'bstfun'),
-      filename = glue::glue("{folder_name}.Rproj"),
-      copy = TRUE
-    ),
-    # only add Rprofile if renv was used
-    rprofile =
-      switch(
-        renv,
-        list(
-          template_filename =
-            fs::path_package(package = "starter", "project_templates/default_rprofile.R"),
-          filename = stringr::str_glue(".Rprofile"),
-          glue = TRUE
-        )
-      )
-  ))
+    ))
+  )
 attr(leej22_project_template, "script_path") <-
   expression(fs::path_package("project_templates/leej22_template_files/write_directories.R", package = 'bstfun'))
 attr(leej22_project_template, "label") <- "Jasme Lee Project Template"
--- inst/project_templates/hot_template_files/hot_readme.md
+++
@@ -1,8 +0,0 @@
-# {{folder_name}}
-
-## Symbolic Link to Secure Data
-{{symbolic_link}}
-
-## Project Log
-**{{Sys.Date()}}**  
-Created project folder
