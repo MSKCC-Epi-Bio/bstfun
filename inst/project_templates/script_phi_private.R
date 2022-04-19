535e8779e770fe41e36198d53e5260419f9b0133
Daniel Sjoberg <danield.sjoberg@gmail.com>
2022-04-19 16:00:58 -0400

    Update script_phi_private.R

--- inst/project_templates/script_phi_private.R
+++ inst/project_templates/script_phi_private.R
@@ -1,4 +1,4 @@
-# if repo is hosted in an org allowed tohost PHI do the following:
+# if repo is hosted in an org allowed to host PHI do the following:
 # 1. Set repo to private
 # 2. Add phi topic hashtag
 
@@ -12,18 +12,19 @@ tryCatch(
         dplyr::pull(.data$url)
 
       # if repo is in a PHI org, then make repo private and add PHI tag
-      if (biostatR::is_phi_repo(remote_origin_url) && rlang::is_string(remote_origin_url)) {
-        repo_name <- biostatR::get_repo_name(remote_origin_url)
-        org_name <- biostatR::get_org_name(remote_origin_url)
+      if (rlang::is_string(remote_origin_url) && mskRutils::is_phi_repo(remote_origin_url)) {
+        repo_name <- mskRutils::get_repo_name(remote_origin_url)
+        org_name <- mskRutils::get_org_name(remote_origin_url)
 
         mskRutils::use_github_msk_phi_repo(repo_name, org_name)
-        mskRutils::use_github_msk_private_repo(repo_name, org_name)
+        mskRutils::use_github_msk_private_repo(repo_name, org_name, quiet_configured = TRUE)
       }
     }
   },
   error = function(e) {
     message(paste("Unable to set repository to 'private' and add 'phi' tag.",
                   "You must set these manually NOW if the project lives in a GH Org that allows PHI."))
+    message(as.character(e))
   }
 )
 
