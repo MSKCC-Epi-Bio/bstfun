a7bf56a73c2238f96aa884028d9919932e8d1754
Daniel Sjoberg <danield.sjoberg@gmail.com>
2022-05-19 11:49:48 -0400

    Update test-add_sparkline.R

--- tests/testthat/test-add_sparkline.R
+++ tests/testthat/test-add_sparkline.R
@@ -13,15 +13,6 @@ test_that("add_sparkline() works", {
   )
 
   expect_error(
-    tbl %>% add_sparkline(type = "histogram"),
-    NA
-  )
-  expect_error(
-    tbl %>% add_sparkline(type = "sparkline"),
-    NA
-  )
-
-  expect_error(
     tbl %>% add_sparkline(column_header = letters)
   )
 
