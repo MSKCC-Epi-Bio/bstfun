5f5ff5aeb751fbd3abd941738ed8b0bda305cb3c
Lin <sabrina.t.lin@gmail.com>
2023-10-12 09:34:31 -0400

    adding in expect_error for passing a non gt, flexible, huxtable, or knitr::kable object through style_tbl_compact

--- tests/testthat/test-style_tbl_compact.R
+++ tests/testthat/test-style_tbl_compact.R
@@ -26,4 +26,12 @@ test_that("no errors with typical use", {
       style_tbl_compact(),
     NA
   )
+
+  expect_error(
+    head(trial) %>%
+      style_tbl_compact(),
+    "`data=` must be a {gt}, {flextable}, {huxtable}, or `knitr::kable()` table.", fixed = TRUE
+  )
+
+
 })
